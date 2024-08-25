18480adf4a50ee6995de01a9fb05eff972721898
leandrossce <44346362+leandrossce@users.noreply.github.com>
2024-08-25 09:44:27 -0300

    Update backup_recursivo_hash.py
    Atualizado feedback de processamento e bugs referentes a arquivos somente leitura no destino do backup.

--- backup_recursivo_hash.py
+++ backup_recursivo_hash.py
@@ -3,6 +3,24 @@ import shutil
 import hashlib
 from datetime import datetime, timedelta
 
+def remove_readonly_recursively(directory):
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            file_path = os.path.join(root, file)
+            try:
+                # Verifica os atributos do arquivo
+                attributes = os.stat(file_path).st_mode
+                
+                # Remove o atributo de "somente leitura" se estiver ativo
+                if not attributes & 0o200:  # Verifica se o bit de escrita está desativado
+                    os.chmod(file_path, attributes | 0o200)  # Ativa o bit de escrita
+                    print(f"Atributo 'somente leitura' removido: {file_path}")
+                #else:
+                    #print(f"O arquivo já tem permissão de escrita: {file_path}")
+            except Exception as e:
+                print(f"Erro ao processar o arquivo {file_path}: {e}")
+                continue
+
 def get_last_backup_time(backup_dir):
     """Recuperar a hora do último backup de um arquivo."""
     last_backup_file = os.path.join(backup_dir, 'tempo_do_ultimo_backup.txt')
@@ -33,15 +51,20 @@ def count_total_files(source_dir):
     return total_files
 
 def backup_modified_files(source_dir, backup_dir):
+    print("Iniciando o processamento. Aguarde!...")
+
+    remove_readonly_recursively (backup_dir)  #remover "somente leitura" no destino (bkp)    
+    
     """Arquivos de backup modificados desde o último backup, preservando a estrutura de diretórios."""
     last_backup_time = get_last_backup_time(backup_dir)
     if last_backup_time is None:
         last_backup_time = datetime.min
 
     error_files = []
-
+    atualizados_files = []
+    
     total_files = count_total_files(source_dir)
-    processed_files = 0
+    #processed_files = 0
 
     start_time = datetime.now()
     
@@ -52,6 +75,7 @@ def backup_modified_files(source_dir, backup_dir):
             os.makedirs(backup_subdir)
 
         for filename in filenames:
+            print("Analisando: " + source_dir+"\\"+filename)            
             source_file = os.path.join(foldername, filename)
             backup_file = os.path.join(backup_subdir, filename)
 
@@ -67,15 +91,18 @@ def backup_modified_files(source_dir, backup_dir):
                 if source_hash != backup_hash:
                     shutil.copy2(source_file, backup_file)
                     print(f'\rBackup atualizado para: {filename}')
-
+                    print(f'\r{backup_file} foi atualizado no backup...\n', end='')  
+                    atualizados_files.append(backup_file)   
+                    
                 # Atualizar o contador de arquivos processados e mostrar progresso
-                processed_files += 1
-                progress = (processed_files / total_files) * 100
-                print(f'\rProgresso: {progress:.2f}%', end='')
+                #processed_files += 1
+                #progress = (processed_files / total_files) * 100
+                #print(f'\rProgresso: {progress:.2f}%', end='')
 
-            except PermissionError:
-                print(f'Erro de permissão: não foi possível copiar {source_file}')
-                error_files.append(source_file)
+            except Exception as e:
+                print(f'Erro do tipo: {e} em {source_file}')		
+                error_files.append(source_file)				
+                continue
 
     # Atualiza a data do último backup
     set_last_backup_time(backup_dir)
@@ -94,8 +121,20 @@ def backup_modified_files(source_dir, backup_dir):
     duration_in_s = duration.total_seconds()
     hours, remainder = divmod(duration_in_s, 3600)
     minutes, seconds = divmod(remainder, 60)
+    print("\nArquivos com Erros: ".join(error_files))
+    print("\nArquivos atualizados no backup: ".join(atualizados_files))    
     print(f'\nBackup concluído em {int(hours)} horas, {int(minutes)} minutos e {int(seconds)} segundos.')
 
+    # Abrir o arquivo em modo de escrita
+    with open(backup_dir+"/arquivos_erros.txt", 'w') as file:
+        for item in error_files:
+            file.write(f"\n{item}\n")  # Grava cada item do array em uma nova linha
+
+    # Abrir o arquivo em modo de escrita
+    with open(backup_dir+"/arquivos_atualizados.txt", 'w') as file:
+        for item in atualizados_files:
+            file.write(f"\n{item}\n")  # Grava cada item do array em uma nova linha  
+            
 # Solicitar ao usuário os diretórios de origem e destino
 source_directory = input("Digite o caminho completo do diretório de origem: ")
 backup_directory = input("Digite o caminho completo do diretório de backup: ")
